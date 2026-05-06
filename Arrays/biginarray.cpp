d4149b3557876c4cd099e76d99da3b4dd5bf10f7
Notanormaldev <harshpatel20050@gmail.com>
2026-05-06 21:45:33 +0530

    biggest ele in array

--- Arrays/biginarray.cpp
+++ Arrays/biginarray.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-
+#include <climits>
 
 int main(){
     int array[]={10,20,50,-10,20,44};
@@ -8,8 +8,11 @@ int main(){
 
     int big=INT_MIN;
     for(int i=0;i<size;i++){
+        if(array[i]>big){
+            big=array[i];
+        }
     }
-
+    cout<<big;
 
     return 0;
 }
