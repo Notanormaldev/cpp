f7eade37685253e68d294216876c449a50536ead
Notanormaldev <harshpatel20050@gmail.com>
2026-09-14 14:39:38 +0530

    feat: add C++ program to print numbers from 1 to N using recursion

--- Recursion/1tonprint.cpp
+++ Recursion/1tonprint.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+// int c=1;
 void fun(int i,int n){
     if(i>n) return;
     cout<<i<<endl;
     cout<<"doremon"<<endl;
+    // c++;
+    
     fun(i+1,n);
 }
 
@@ -16,3 +18,6 @@ int main(){
     fun(1,n);
     return 0;
 }
+
+//first appr - fun(1,n) two vars
+//sec appr - count type and count++
