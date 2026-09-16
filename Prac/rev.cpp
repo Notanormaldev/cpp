a8cd94bf49a25cb01ce32c6e4d4f76adb9bc01f6
Notanormaldev <harshpatel20050@gmail.com>
2026-09-16 20:38:28 +0530

    feat: add function to reverse an integer in C++

---
+++ Prac/rev.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int  rev(int n){
+
+    long long rev=0;
+
+    while(n!=0){
+       int lastdgt=n%10;
+       rev=rev*10+lastdgt;
+       n=n/10;
+    }
+    if(rev<INT_MIN  && rev>INT_MAX) return 0;
+    return rev;
+}
+
+int main(){
+    int n=-1234;
+    cout<<rev(n);
+    return 0;
+}
