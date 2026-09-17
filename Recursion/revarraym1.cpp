d241bc553fdb218ca994e488735f20a46380afe8
Notanormaldev <harshpatel20050@gmail.com>
2026-09-17 10:15:34 +0530

    feat: add recursive array reversal implementation

---
+++ Recursion/revarraym1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void revarr(int arr[],int l ,int r){
+    if(l>=r) return;
+    swap(arr[l],arr[r]);
+    revarr(arr,l+1,r-1);  
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    revarr(arr,0,n-1);
+    for(int i=0;i<n;i++) cout<<arr[i]<<" "; 
+    return 0;
+}
