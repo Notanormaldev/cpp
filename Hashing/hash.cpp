7aa243f94d9fa0f9649e38c1020917f18bb14d2b
Notanormaldev <harshpatel20050@gmail.com>
2026-09-22 15:47:41 +0530

    feat: add basic array-based hashing implementation in C++

---
+++ Hashing/hash.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+
+   //pre stroing
+   int hash[12]={0};
+   for(int i=0;i<n;i++){
+   hash[arr[i]]+=1;
+}
+
+
+    int q;
+    cin>>q;
+
+    while(q--){
+        int num;
+        cin>>num;
+        //fetch
+        cout<<hash[num]<<endl;
+    }
+ return 0;
+}
