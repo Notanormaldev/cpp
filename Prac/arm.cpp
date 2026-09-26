a6f91eedad7e14ac10a67cb056324215b7c62b4f
Notanormaldev <harshpatel20050@gmail.com>
2026-09-26 21:49:40 +0530

    feat: add Armstrong number checker implementation in C++

---
+++ Prac/arm.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+  int n;
+  cin>>n;
+
+  int og=n;
+  int sum=0;
+
+while(n>0){
+    int ld=n%10;
+    sum=sum+ld*ld*ld;
+    n=n/10;
+  }
+  if(og==sum) cout<<"arm";
+  else cout<<"not arm";
+
+}
